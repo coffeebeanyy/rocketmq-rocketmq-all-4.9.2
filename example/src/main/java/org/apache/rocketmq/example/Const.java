31f2bf9e0edae6ba28dcf9bbb9dce34adb7412da
yangyang <coffeebeanyy@gmail.com>
2021-11-09 23:50:57 +0800

    项目初始化

---
+++ example/src/main/java/org/apache/rocketmq/example/Const.java
@@ -0,0 +1,6 @@
+package org.apache.rocketmq.example;
+
+public class Const {
+    public static final String NAME_SRV = "192.168.0.107:9876";
+    public static final String NAME_SRV_LOCAL = "localhost:9876";
+}
